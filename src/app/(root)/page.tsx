0244415da4b149d97e84ce08475f9d9b02b69056
Priyank Khanna <priyank.khanna@oracle.com>
2025-05-07 02:49:36 +0530

    updated navbar with signin and signout option

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+//import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      Home
+        <h1 className="text-2xl">Home</h1>
     </>
   );
 }
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
+import Link from "next/link";
+import Image from "next/image";
+import {auth, signIn, signOut} from "@/auth";
 
-const Navbar = () => {
+
+
+const Navbar = async () => {
+    const session = await auth();
+
     return (
-        <div>
-            Navbar
-        </div>
+        <header className="px-5 py-5 bg-white shadow-sm font-work-sans">
+            <nav className="flex justify-between items-center">
+               <Link href="/">
+                   <Image src="/logo.png" alt="logo" width={144} height={30} />
+               </Link>
+
+                <div className="flex justify-center gap-5 text-black">
+                    {session && session?.user ? (
+                        <>
+                            <Link href="/startup/create">
+                                <span>Create</span>
+                            </Link>
+                            <form action={async () => {
+                                "use server";
+                                await signOut({ redirectTo:"/"});
+                            }}>
+                                <button type="submit"><span>logout</span></button>
+                            </form>
+                            <Link href={`/user/${session?.id}`}>
+                                <span>{session?.user?.name}</span>
+                            </Link>
+                        </>
+                    ):(
+                        <form action={async () => {
+                            "use server";
+                            await signIn('github');
+                        }}>
+                            <button type="submit">
+                                <span>Login</span>
+                            </button>
+                        </form>
+                    )
+                    }
+                </div>
+            </nav>
+        </header>
     );
 };
 
