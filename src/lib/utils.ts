2669310fe81f17e4ab622e53e0c79d1c41d05273
Priyank Khanna <priyank.khanna@oracle.com>
2025-05-12 22:58:45 +0530

    updated shadcn button and formatDate

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,11 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export function formatDate(date: string){
-  return new Date(date).toLocaleDateString("en-US", {
+export function formatDate(date: Date) {
+  return date.toLocaleDateString("en-US", {
     year: "numeric",
     month: "long",
     day: "numeric",
   })
+
 }
